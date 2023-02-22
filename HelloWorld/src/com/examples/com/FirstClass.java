b61cbfbbaf323eb5d14774077fa2413df78d31f3
SathyaCodes123 <124946414+SathyaCodes123@users.noreply.github.com>
2023-02-22 21:05:12 +0530

    Update FirstClass.java

--- HelloWorld/src/com/examples/com/FirstClass.java
+++ HelloWorld/src/com/examples/com/FirstClass.java
@@ -2,7 +2,10 @@ package com.examples.com;
 
 public class FirstClass {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("Hello World");
 		// TODO Auto-generated method stub
 
 	}
